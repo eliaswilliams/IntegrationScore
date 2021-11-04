056ef686e0106a6997b265ec2637ee254cbb0aad
Elias Williams <eliaswilliams@Eliass-MacBook-Pro-2.local>
2021-11-04 14:16:20 -0400

    updated documentation

---
+++ R/findDEG.R
@@ -0,0 +1,70 @@
+#' Calculates the Pre/Post Batch Correction Differentially Expressed Genes
+#'
+#' Creates a data frame with the DEGs of each batch pre and post correction.
+#'
+#' @param srtObjPre Seurat object of the uncorrected data
+#' @param srtObjPost Seurat object of the corrected data
+#' @param batchNames character vector of length 2 with the batch names
+#' @param clusterVars character vector of the cluster columns in Seurat objects,
+#'                    these must be clusters obtained prior to batch correction
+#'                    on each batch separately
+#' @param minPct minimum percent for Seurat::FindMarkers
+#' @param logFCThresh threshold for Seurat::FindMarkers
+#'
+#' @return data frame with DEG information for each batch pre and
+#'         post correction
+#'
+#' @export
+findDEG <- function(srtObjPre, srtObjPost, batchNames, clusterVars, minPct, logFCThresh) {
+        # split the original expression object into batches
+        batch1Pre <- subset(srtObjPre, subset = batch==batchNames[1])
+        batch2Pre <- subset(srtObjPre, subset = batch==batchNames[2])
+
+        # split the corrected expression object into batches
+        batch1Post <- subset(srtObjPost, subset = batch==batchNames[1])
+        batch2Post <- subset(srtObjPost, subset = batch==batchNames[2])
+
+        # make sure objects' idents are the clusterVar
+        Idents(batch1Pre) <- clusterVars[1]
+        Idents(batch2Pre) <- clusterVars[2]
+        Idents(batch1Post) <- clusterVars[1]
+        Idents(batch2Post) <- clusterVars[2]
+
+        # find DEGs for all subsets using the given parameters
+        batch1PreDEG = Seurat::FindAllMarkers(batch1Pre, min.pct=minPct,
+                                              logfc.threshold=logFCThresh)
+        batch2PreDEG = Seurat::FindAllMarkers(batch2Pre, min.pct=minPct,
+                                              logfc.threshold=logFCThresh)
+        batch1PostDEG = Seurat::FindAllMarkers(batch1Post, min.pct=minPct,
+                                               logfc.threshold=logFCThresh)
+        batch2PostDEG = Seurat::FindAllMarkers(batch2Post, min.pct=minPct,
+                                               logfc.threshold=logFCThresh)
+
+        # build data frame
+        batch1PreDEG = batch1PreDEG %>% select(gene, cluster,
+                                               avg_log2FC_pre=avg_log2FC)
+        batch2PreDEG = batch2PreDEG %>% select(gene, cluster,
+                                               avg_log2FC_pre=avg_log2FC)
+        batch1PostDEG = batch1PostDEG %>% select(gene, cluster,
+                                                 avg_log2FC_post=avg_log2FC)
+        batch2PostDEG = batch2PostDEG %>% select(gene, cluster,
+                                                 avg_log2FC_post=avg_log2FC)
+
+        batch1DEG = inner_join(batch1PreDEG, batch1PostDEG,
+                               by=c("cluster", "gene"))
+        batch2DEG = inner_join(batch1PostDEG, batch2PostDEG,
+                               by=c("cluster", "gene"))
+
+        # calculate log2FC delta
+        batch1DEG$delta = batch1DEG$avg_log2FC_pre - batch1DEG$avg_log2FC_post
+        batch2DEG$delta = batch2DEG$avg_log2FC_pre - batch2DEG$avg_log2FC_post
+
+        # finish building data frame and return
+        batch1DEG$batch = batchNames[1]
+        batch2DEG$batch = batchNames[2]
+
+        DEG_df = rbind(batch1DEG, batch2DEG)
+
+        return(DEG_df)
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# IntegrationScore
-The objective of this R package is to allow researchers working with scRNA-seq  data to compare the effectiveness of batch correction methods. 
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# Integration Score
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+## Description
+
+The objective of IntegrationScore is to allow researchers working with
+scRNA-seq data to compare the effectiveness of batch correction
+methods.Several R packages exist that score batch effects in a given
+data set (kBET: <https://github.com/theislab/kBET>, BatchBench:
+<https://academic.oup.com/nar/article/49/7/e42/6125660>). These scores
+are useful metrics of comparison, but only if a given method passes a
+basic sanity check first. That check being: in a given batch, are the
+differentiated expressed genes (DEGs) between clusters somewhat
+invariant under the batch correction method? (i.e. the list of top DEGs
+should not be significantly altered by batch correction)My proposed R
+package will enable scientists to quickly perform this sanity check as
+well as implement these additional metrics and interpret the results
+using clear visualizations.
+
+IntegrationScore was developed on a Mac using R 4.1.1.
+
+## Installation
+
+You can install the development version of IntegrationScore from
+[GitHub](https://github.com/) with:
+
+``` r
+require("devtools")
+devtools::install_github("eliaswilliams/IntegrationScore")
+library("IntegrationScore")
+```
+
+To run the shinyApp: Under construction
+
+## Overview
+
+## Contributions
+
+## References
+
+## Acknowledgements
+
+This package was developed as part of an assessment for 2021 BCB410H:
+Applied Bioinfor-matics, University of Toronto, Toronto, CANADA.
